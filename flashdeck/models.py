e97cd1cb5bbebf9986c6a478225f13f1e5051c57
jovyp <jovips1@gmail.com>
2024-10-26 20:52:35 -0400

    django setup, stopped at database modeling part. Need Joshua or Emma to decide how these should be modeled.

---
+++ flashdeck/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Deck(models.Model):
+    print("need emma/joshua to work this out");
+
+
+class Card(models.Model):
+    print("need emma/joshua to work this out");
+
+
+
