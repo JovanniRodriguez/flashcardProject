408ba26524dc5545610b58b24cddac021179a1bd
jovyp <jovips1@gmail.com>
2024-11-24 22:23:42 -0500

    redid my-decks.html based off of figma redesign. Needs a few more changes

---
+++ flashdeck/static/flashdeck/js/myDecks.js
@@ -0,0 +1,17 @@
+// flashdeck/js/MyDecks.js
+
+console.log('testing 123');
+var createDeckButton = document.getElementById('createDeckButton');
+
+
+
+
+//add deck card when clicking create button
+createDeckButton.addEventListener('click', function(e){
+    e.preventDefault()
+    alert('test')
+})
+
+
+
+
