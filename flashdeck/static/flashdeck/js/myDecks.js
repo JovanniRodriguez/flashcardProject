12206e283c020547f03524455c410c2baf89a46d
jovyp <jovips1@gmail.com>
2024-11-25 22:02:41 -0500

    changed myDecks modal stuff based on what was talked about. Not 100% finished yet but a lot more functional.

--- flashdeck/static/flashdeck/js/myDecks.js
+++ flashdeck/static/flashdeck/js/myDecks.js
@@ -1,19 +1,59 @@
 // flashdeck/js/MyDecks.js
 
-var createDeckButton = document.getElementById('createDeckButton');
-var cardGroup = document.getElementById('card-group');
+var saveDeckButton = document.getElementById('saveDeckButton');
+var cardGrid = document.getElementById('cardGrid');
 var card = document.querySelector('.card');
-
+var nameInput = document.getElementById('nameInput');
+var descInput = document.getElementById('descInput');
 
 
 //add deck card when clicking create button
-createDeckButton.addEventListener('click', function(e){
-    e.preventDefault()
-    var newCard = card.cloneNode(true);
-    cardGroup.appendChild(newCard)
+saveDeckButton.addEventListener('click', function(e){
+    e.preventDefault();
+
+    var newCardCol = document.createElement('div');
+    newCardCol.classList = 'col-md-4 mb-4';
+
+
+    var newCard = document.createElement('div');
+    newCard.classList = 'card mx-4 py-4';
+    newCard.setAttribute('data-bs-toggle', 'modal');
+    newCard.setAttribute('data-bs-target', '#cardOptionsModal');
+
+    var newCardBody = document.createElement('div');
+    newCardBody.classList = 'card-body text-center';
     
+    var h5 = document.createElement('h5');
+    h5.classList = 'card-title';
+    if(nameInput.value == '')
+        h5.textContent = 'New Deck';
+    else
+        h5.textContent = nameInput.value;  //grab from create modal textContent fields
+
+    var p = document.createElement('p');
+    p.classList = 'card-text';
+    if(descInput.value == '')
+        p.textContent = 'No Description';
+    else
+        p.textContent = descInput.value;   //grab from create modal textContent fields
+
+    newCardBody.appendChild(h5);
+    newCardBody.appendChild(p);
+    newCard.appendChild(newCardBody);
+    newCardCol.appendChild(newCard);
+    cardGrid.appendChild(newCardCol);
+
+    nameInput.value = '';
+    descInput.value = '';
 })
 
 
 
-
+{/* <div class="col-md-4 mb-4">
+        <div class="card mx-4 py-4" data-bs-toggle="modal" data-bs-target="#cardOptionsModal">
+          <div class="card-body text-center">
+            <h5 class="card-title">Example Deck</h5>
+            <p class="card-text">description</p>
+          </div>
+        </div>
+      </div> */}
